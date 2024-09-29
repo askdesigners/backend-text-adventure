51c0f7baff1e8e3614d284474038d7c836779f41
Ryan Cole <ryan@askdesigners.eu>
2024-09-29 10:44:08 +0200

    tool to grab files from chat gpt

---
+++ tools/chatGPTCodeDownloader.js
@@ -0,0 +1,21 @@
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+const codeNodes = document.getElementsByTagName('code');
+
+for (let node of codeNodes) {
+  if (node.innerText && node.innerText.includes('class')) {
+    const className = node.innerText.match(/class\s(.*)\sextends/)[1];
+    download(className + '.js', node.innerText);
+  }
+}
